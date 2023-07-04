0e531abaecdd79c22accb1b016c47cecfd3ba208
Kate2030 <tckabikwa@yahoo.com>
2023-07-04 07:13:26 -0400

    Changes

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -5,23 +5,30 @@ from django.utils.timezone import now
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
+class CarMake(models.Model):
+    Name = models.CharField(null=False, max_length=25)
+    Description = models.CharField(null=False, max_length=250)
+   
 # - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+def __str__(self):
+    return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+     Van = 'Van'
+     Suv = 'Suv'
+     Combi = 'Combi'
+     Hatchback = 'Hatchback'
+     Sedan = 'Sedan'
+     Sport = 'Sport'
+     
+     Car_choices = [(Van,'Van'), (Suv,'Suv'),(Combi,'Combi'),(Hatchback,'Hatchback'),(Sedan,'Sedan'),
+     (Sport,'Sport')]
 
+def __str__(self):
+    return self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
